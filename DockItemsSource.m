8c0dad69b400d9aa357fe4fc11618c5a5f46c704
Martin Kuehl <martin.kuehl@gmail.com>
2009-09-04 07:20:33 +0200

    Index subpaths of folders in the Dock as well.

--- DockItemsSource.m
+++ DockItemsSource.m
@@ -13,6 +13,8 @@ static NSString *const kDockItemPathKey = @"tile-data.file-data._CFURLString";
 
 @interface DockItemsSource : HGSMemorySearchSource
 - (void)recacheContents;
+- (void)recacheContentsAfterDelay:(NSTimeInterval)delay;
+- (void)indexItemAtPath:(NSString *)path;
 @end
 
 @implementation DockItemsSource
@@ -22,13 +24,10 @@ static NSString *const kDockItemPathKey = @"tile-data.file-data._CFURLString";
   self = [super initWithConfiguration:configuration];
   if (self == nil)
     return nil;
-  if (![self loadResultsCache]) {
+  if ([self loadResultsCache])
+    [self recacheContentsAfterDelay:10.0];
+  else
     [self recacheContents];
-  } else {
-    [self performSelector:@selector(recacheContents)
-               withObject:nil
-               afterDelay:10.0];
-  }
   return self;
 }
 
@@ -37,15 +36,31 @@ static NSString *const kDockItemPathKey = @"tile-data.file-data._CFURLString";
   [self clearResultIndex];
   NSDictionary *settings = [[NSUserDefaults standardUserDefaults]
                             persistentDomainForName:kDockBundleIdentifier];
-  for (NSDictionary *item in [settings valueForKey:kDockItemsKey]) {
-    NSString *path = [item valueForKeyPath:kDockItemPathKey];
-    [self indexResult:[HGSResult resultWithFilePath:path
+  for (NSDictionary *item in [settings valueForKey:kDockItemsKey])
+    [self indexItemAtPath:[item valueForKeyPath:kDockItemPathKey]];
+  [self recacheContentsAfterDelay:60.0];
+}
+
+- (void)recacheContentsAfterDelay:(NSTimeInterval)delay
+{
+  [self performSelector:@selector(recacheContents)
+             withObject:nil
+             afterDelay:delay];
+}
+
+- (void)indexItemAtPath:(NSString *)path
+{
+  [self indexResult:[HGSResult resultWithFilePath:path
+                                           source:self
+                                       attributes:nil]];
+  NSFileManager *manager = [NSFileManager defaultManager];
+  for (NSString *subpath in [manager directoryContentsAtPath:path]) {
+    if ([subpath hasPrefix:@"."]) continue;
+    subpath = [path stringByAppendingPathComponent:subpath];
+    [self indexResult:[HGSResult resultWithFilePath:subpath
                                              source:self
                                          attributes:nil]];
   }
-  [self performSelector:@selector(recacheContents)
-             withObject:nil
-             afterDelay:60.0];
 }
 
 @end
